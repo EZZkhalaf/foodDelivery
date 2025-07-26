bcfd996a4745719db2f20f18a0535e9575b72384
EZZkhalaf <ezz.awni@gmail.com>
2025-07-26 12:18:26 +0300

    ..

--- app/index.tsx
+++ app/index.tsx
@@ -35,18 +35,30 @@
 
 
 
-import { offers } from "@/components";
+import { images, offers } from "@/components";
 import cn from 'clsx'; // okay if used with NativeWind
 import { Fragment } from "react";
-import { FlatList, Image, Pressable, Text, View } from "react-native";
+import { FlatList, Image, Pressable, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
     <SafeAreaView className="flex-1 bg-white">
-      <FlatList 
+      <FlatList
         data={offers}
         keyExtractor={(_, index) => index.toString()}
+        ListHeaderComponent={
+          <View className="flex-row justify-between w-full my-5 px-5">
+            <View>
+              <Text className="text-primary small-bold">Deliver To</Text>
+              <TouchableOpacity className="flex-row items-center mt-0.5">
+                <Text className="font-bold text-dark-100">Jordan</Text>
+                <Image source={images.arrowDown} className="w-3 h-3 ml-1" resizeMode="contain" />
+              </TouchableOpacity>
+            </View>
+            <Text>Cart</Text>
+          </View>
+        }
         renderItem={({ item, index }) => {
           const isEven = index % 2 === 0;
           return (
@@ -59,15 +71,13 @@ export default function Index() {
                   <Fragment>
                     <View className="h-full w-1/2">
                       <Image
-                        source={typeof item.image === 'string' ? { uri: item.image } : item.image}
+                        source={typeof item.image === "string" ? { uri: item.image } : item.image}
                         className="w-full h-full"
                         resizeMode="contain"
                       />
                     </View>
-                    <View className={"offer-card_info items-center justify-center"}>
-                      <Text className="text-lg font-bold text-white">
-                        {item.title}
-                      </Text>
+                    <View className="offer-card_info items-center justify-center">
+                      <Text className="text-lg font-bold text-white">{item.title}</Text>
                     </View>
                   </Fragment>
                 )}
@@ -75,9 +85,9 @@ export default function Index() {
             </View>
           );
         }}
-
-        contentContainerClassName="pb=25 px-5"
+        contentContainerClassName="pb-20"
       />
     </SafeAreaView>
+
   );
 }
